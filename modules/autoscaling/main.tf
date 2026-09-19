8e56775c9d0cf16768e20de65728552060aeffa8
sandy976336 <137062505+sandy976336@users.noreply.github.com>
2026-09-20 00:37:54 +0530

    Update AMI filter to use Ubuntu Jammy 22.04

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-jammy-22.04-amd64-server-*"]
   }
   owners = ["099720109477"]
 }
